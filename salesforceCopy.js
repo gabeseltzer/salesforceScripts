97c7f68504e76c6d00c6638c219bebb0ff817dd6
Gabriel Seltzer <gabeseltzer@gmail.com>
2021-05-19 13:23:07 -0400

    I have no idea what I am doing

---
+++ salesforceCopy.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/desrod/browser-scripts-misc/blob/0ea454c31787774d9cc25b48f259ce1f0f49075e/salesforce-lightning-tweaks.user.js
+// @icon         https://www.google.com/s2/favicons?domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert("hello world");
+})();
