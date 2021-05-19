7d73fc4fc51f06f05063fc11639d9bdc0052c711
Gabriel Seltzer <gabeseltzer@gmail.com>
2021-05-19 13:29:21 -0400

    trying to get this thing to activate

--- salesforceCopy.js
+++ salesforceCopy.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         New Userscript
-// @namespace    http://tampermonkey.net/
+// @namespace    http://salesforce.com/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  copy important data out of Salesforce easily
+// @include      /^https?://.*lightning.force\.com/.*$/
+// @author       Gabriel Seltzer
 // @match        https://github.com/desrod/browser-scripts-misc/blob/0ea454c31787774d9cc25b48f259ce1f0f49075e/salesforce-lightning-tweaks.user.js
 // @icon         https://www.google.com/s2/favicons?domain=github.com
+// @version      0.1
 // @grant        none
 // ==/UserScript==
 
