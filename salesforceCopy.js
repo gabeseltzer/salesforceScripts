e10e1ef63f739be6e47ed141ed628ce033ef1a6f
Gabriel Seltzer <gabeseltzer@gmail.com>
2021-05-19 13:45:50 -0400

    cleaned up match

--- salesforceCopy.js
+++ salesforceCopy.js
@@ -2,9 +2,8 @@
 // @name         Salesforce Copy
 // @namespace    http://salesforce.com/
 // @description  copy important data out of Salesforce easily
-// @include      /^https?://.*lightning.force\.com/.*$/
+// @match        /^https?://.*lightning.force\.com/.*$/
 // @author       Gabriel Seltzer
-// @match        https://github.com/desrod/browser-scripts-misc/blob/0ea454c31787774d9cc25b48f259ce1f0f49075e/salesforce-lightning-tweaks.user.js
 // @icon         https://www.google.com/s2/favicons?domain=github.com
 // @updateURL    https://raw.githubusercontent.com/Memnarch1113/salesforceScripts/main/salesforceCopy.js
 // @downloadURL  https://raw.githubusercontent.com/Memnarch1113/salesforceScripts/main/salesforceCopy.js
